b9dcd4c59ade743644c5dc3dbb4712fbb50c7000
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-22 05:35:03 -0600

    Expose R06 CHAR_INFO serialization

--- rust/terminal-host/src/lib.rs
+++ rust/terminal-host/src/lib.rs
@@ -6,6 +6,7 @@ pub mod attribute_format;
 pub mod console_argument_parser;
 pub mod console_arguments;
 pub mod pty_signal;
+pub mod vt_char_info;
 pub mod vt_io_protocol;
 pub mod vt_io_state;
 pub mod vt_writer_sequences;
