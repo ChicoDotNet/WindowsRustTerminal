0f04621ccbf4d52056e32998a807643c63419976
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-22 17:34:19 -0600

    R07b: expose render title state policy

--- rust/terminal-renderer/src/lib.rs
+++ rust/terminal-renderer/src/lib.rs
@@ -1,5 +1,7 @@
 #![forbid(unsafe_code)]
 
 mod css_length_percentage;
+mod title_state;
 
 pub use css_length_percentage::{CssLengthPercentage, ReferenceFrame};
+pub use title_state::{TitleState, TitleUpdate};
