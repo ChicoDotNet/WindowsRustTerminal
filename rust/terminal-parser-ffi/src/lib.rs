2feb001cd8db11c19a64be57e4c619f595ac5cc0
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-09-04 07:30:15 -0600

    Register CSI soft reset replay

--- rust/terminal-parser-ffi/src/lib.rs
+++ rust/terminal-parser-ffi/src/lib.rs
@@ -20,6 +20,7 @@ mod output_csi_margins;
 mod output_csi_page;
 mod output_csi_page_position;
 mod output_csi_scroll;
+mod output_csi_soft_reset;
 mod output_csi_tab;
 mod output_csi_terminal_parameters;
 mod output_esc;
