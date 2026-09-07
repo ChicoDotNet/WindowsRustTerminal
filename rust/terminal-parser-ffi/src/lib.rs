a20f359f9dbcdb9c08367e9c63cee67d4505c220
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-09-06 19:29:07 -0600

    Register CSI user preference charset replay

--- rust/terminal-parser-ffi/src/lib.rs
+++ rust/terminal-parser-ffi/src/lib.rs
@@ -38,6 +38,7 @@ mod output_csi_soft_reset;
 mod output_csi_tab;
 mod output_csi_tab_control;
 mod output_csi_terminal_parameters;
+mod output_csi_user_preference_charset;
 mod output_csi_window_manipulation;
 mod output_esc;
 mod output_execute;
