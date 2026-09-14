4f0eabfafc4c89e47b913417a3ea663a0f45c785
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-09-13 23:36:56 -0600

    Run DCS native ABI replay

--- tools/rust/R09ControlCharacterAbiProbe.cpp
+++ tools/rust/R09ControlCharacterAbiProbe.cpp
@@ -4,6 +4,7 @@
 #include "terminal_parser_ffi.h"
 #include "R09PtySignalAbiProbe.hpp"
 #include "R09RenderSettingsAbiProbe.hpp"
+#include "R09OutputDcsAbiProbe.hpp"
 #include "R09OutputCsiColumnAbiProbe.hpp"
 #include "R09OutputCsiCursorAbiProbe.hpp"
 #include "R09OutputCsiCursorRestoreAbiProbe.hpp"
@@ -210,6 +211,7 @@ int main()
 
     const bool ptySignalReadPlanOk = r09::pty_signal_read_plan_replay();
     const bool renderSettingsPolicyOk = r09::render_settings_policy_replay();
+    const bool outputDcsOk = r09::output_dcs_replay();
     const bool outputEscOk = r09::output_esc_replay();
     const bool outputVt52Ok = r09::output_vt52_replay();
     const bool outputCsiColumnOk = r09::output_csi_column_replay();
@@ -255,7 +257,7 @@ int main()
     const bool outputCsiUserPreferenceCharsetOk = r09::output_csi_user_preference_charset_replay();
     const bool outputCsiWindowManipulationOk = r09::output_csi_window_manipulation_replay();
 
-    if (!controlOk || !mouseOk || !outputExecuteOk || !ptySignalReadPlanOk || !renderSettingsPolicyOk || !outputEscOk || !outputVt52Ok || !outputCsiColumnOk || !outputCsiCursorOk || !outputCsiCursorRestoreOk || !outputCsiCursorStyleOk || !outputCsiDecacOk || !outputCsiDecfraOk || !outputCsiDecsaceOk || !outputCsiDecinvmOk || !outputCsiDecpsOk || !outputCsiDecrqcraOk || !outputCsiDecrqtsrOk || !outputCsiDecscaOk || !outputCsiDeviceAttributesOk || !outputCsiDeviceStatusReportOk || !outputCsiDisplayedExtentOk || !outputCsiMarginsOk || !outputCsiEditOk || !outputCsiEraseOk || !outputCsiEraseCharactersOk || !outputCsiKittyKeyboardPopOk || !outputCsiKittyKeyboardPushOk || !outputCsiKittyKeyboardQueryOk || !outputCsiKittyKeyboardSetOk || !outputCsiLineEditOk || !outputCsiModeOk || !outputCsiPageOk || !outputCsiPagePositionOk || !outputCsiPopSgrOk || !outputCsiPushSgrOk || !outputCsiRectAttributesOk || !outputCsiRectEraseOk || !outputCsiRepOk || !outputCsiRequestModeOk || !outputCsiRequestPresentationStateOk || !outputCsiScrollOk || !outputCsiSgrOk || !outputCsiSoftResetOk || !outputCsiTabOk || !outputCsiTabControlOk || !outputCsiTerminalParametersOk || !outputCsiUserPreferenceCharsetOk || !outputCsiWindowManipulationOk)
+    if (!controlOk || !mouseOk || !outputExecuteOk || !ptySignalReadPlanOk || !renderSettingsPolicyOk || !outputDcsOk || !outputEscOk || !outputVt52Ok || !outputCsiColumnOk || !outputCsiCursorOk || !outputCsiCursorRestoreOk || !outputCsiCursorStyleOk || !outputCsiDecacOk || !outputCsiDecfraOk || !outputCsiDecsaceOk || !outputCsiDecinvmOk || !outputCsiDecpsOk || !outputCsiDecrqcraOk || !outputCsiDecrqtsrOk || !outputCsiDecscaOk || !outputCsiDeviceAttributesOk || !outputCsiDeviceStatusReportOk || !outputCsiDisplayedExtentOk || !outputCsiMarginsOk || !outputCsiEditOk || !outputCsiEraseOk || !outputCsiEraseCharactersOk || !outputCsiKittyKeyboardPopOk || !outputCsiKittyKeyboardPushOk || !outputCsiKittyKeyboardQueryOk || !outputCsiKittyKeyboardSetOk || !outputCsiLineEditOk || !outputCsiModeOk || !outputCsiPageOk || !outputCsiPagePositionOk || !outputCsiPopSgrOk || !outputCsiPushSgrOk || !outputCsiRectAttributesOk || !outputCsiRectEraseOk || !outputCsiRepOk || !outputCsiRequestModeOk || !outputCsiRequestPresentationStateOk || !outputCsiScrollOk || !outputCsiSgrOk || !outputCsiSoftResetOk || !outputCsiTabOk || !outputCsiTabControlOk || !outputCsiTerminalParametersOk || !outputCsiUserPreferenceCharsetOk || !outputCsiWindowManipulationOk)
     {
         return 1;
     }
