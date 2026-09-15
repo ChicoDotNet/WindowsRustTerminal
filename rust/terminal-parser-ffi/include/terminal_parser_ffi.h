b8738b78a9bf5d89f1efe5b6926e3e16f1b313dc
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-09-15 04:26:30 -0600

    Expose OSC state machine callback

--- rust/terminal-parser-ffi/include/terminal_parser_ffi.h
+++ rust/terminal-parser-ffi/include/terminal_parser_ffi.h
@@ -80,6 +80,7 @@ typedef bool (*terminal_parser_ffi_state_machine_print_callback)(void* user_data
 typedef bool (*terminal_parser_ffi_state_machine_print_string_callback)(void* user_data, const uint16_t* text, size_t text_len);
 typedef bool (*terminal_parser_ffi_state_machine_esc_callback)(void* user_data, uint64_t id);
 typedef bool (*terminal_parser_ffi_state_machine_csi_callback)(void* user_data, uint64_t id, const int32_t* values, const uint8_t* present, size_t parameter_count);
+typedef bool (*terminal_parser_ffi_state_machine_osc_callback)(void* user_data, int32_t parameter, const uint16_t* text, size_t text_len);
 
 typedef struct terminal_parser_ffi_state_machine_callbacks
 {
@@ -89,6 +90,7 @@ typedef struct terminal_parser_ffi_state_machine_callbacks
     terminal_parser_ffi_state_machine_print_string_callback print_string;
     terminal_parser_ffi_state_machine_esc_callback esc;
     terminal_parser_ffi_state_machine_csi_callback csi;
+    terminal_parser_ffi_state_machine_osc_callback osc;
 } terminal_parser_ffi_state_machine_callbacks;
 
 uint32_t terminal_parser_ffi_abi_version(void);
